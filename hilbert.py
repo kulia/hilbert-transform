514225dfb51add8bc9c11470f605cee05dee6d71
Geir Kulia <geir@kulia.no>
2017-08-02 18:48:12 +0200

    Remove even num checks for convolution

--- hilbert.py
+++ hilbert.py
@@ -1,9 +1,5 @@
 import numpy as np
-import matplotlib.pylab as plt
-
-from scipy.signal import hilbert as scipy_hilbert
 from numpy import sin, tan, pi
-
 
 def hilbert(x):
 	"""
@@ -18,16 +14,11 @@ def hilbert(x):
 
 	N = len(x)
 
-	if N%2 != 0: N += 1	# Ensure even N
-
 	cot = lambda theta: 1 / tan(theta)
 
 	n = np.arange(2*N)
 	h =  (2/N) * cot(n*pi/N) * (sin(n*pi/2)**2)
 
-	y = np.convolve(x[:N], h, mode='valid')
+	y = np.convolve(x, h, mode='valid')
 
-	if len(y) != len(x):
-		y = y[1:]
-
-	return x + 1j*y
+	return x + 1j*y[-len(x):]
