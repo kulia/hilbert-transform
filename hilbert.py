51d2ccfc200663e5f5fc2704848e300cfbf6e61f
Geir Kulia <geir@kulia.no>
2017-09-01 23:46:37 +0200

    Add j to complex equations

--- hilbert.py
+++ hilbert.py
@@ -6,9 +6,9 @@ def hilbert(x):
 	Compute the analytic signal of :math:`x(t)` using the discrete Hilbert transform with Hanh's method [1].
 	
 	The analytic signal is given by
-	:math:`z(t) = x(t) + \frac{PV}{\pi} \int_{-\inft}^{\inft} \frac{x(t)}{t-\tau}`
+	:math:`z(t) = x(t) + j\frac{PV}{\pi} \int_{-\inft}^{\inft} \frac{x(t)}{t-\tau}`
 	In this function, the discrete analytic signal is estimated using
-	:math:`z[n] = x[n] + \sum_{n=0}^{N-1} h[n-m]x[n]
+	:math:`z[n] = x[n] + j\sum_{n=0}^{N-1} h[n-m]x[n]
 	where
 	:math:`h[n] = \frac{2}{N} \sin^2(\frac{\pi n}{2}) \cot(\frac{\pi n}{N})`
 
